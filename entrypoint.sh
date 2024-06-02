afed7397ae9fce73a298bb92bae0732b1bd00359
Nika Botchorishvili <nikabotchorishvili@gmail.com>
2024-06-03 00:26:18 +0400

    feat: Fixed the docker container issues

---
+++ entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+python manage.py migrate
+
+python manage.py shell << END
+from django.contrib.auth import get_user_model;
+User = get_user_model();
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('demetre', 'chakvetadzedemetre@gmail.com', '12345678')
+END
+
+exec "$@"
